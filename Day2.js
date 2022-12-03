4373f1f836a0a7cead752851c506e2d620551098
ronni söderberg <ronni.soderberg@gmail.com>
2022-12-03 19:12:00 +0100

    Day2

--- Day2.js
+++ Day2.js
@@ -1,32 +1,5 @@
-// Impterar readFilesync, promises som fsPromises från 'fs'
 const { readFileSync, promises: fsPromises } = require('fs')
-// import file
-function ListOfCandElfs (filename) {
-  const contents = readFileSync(filename, 'utf-8')
 
-  const arr = contents.split(/\r?\n/)
-  let elfCount = parseInt(0)
-  let highestCalCount = parseInt(0)
-  let elfCounter = parseInt(1)
-  let elfWithBiggestCalCount = parseInt(0)
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== '') {
-      elfCount = parseInt(arr[i]) + elfCount
-    } else {
-      if (elfCount > highestCalCount) {
-        highestCalCount = elfCount
-        elfWithBiggestCalCount = elfCounter
-        elfCount = 0
-      }
-      elfCounter++
-    }
-  }
-  console.log('alv nummer ' + elfWithBiggestCalCount + 'har en påse med ' + highestCalCount + ' och det är det är den påsen med högst kalorivärde')
-  return arr
-}
-
-// ListOfCandElfs("list.txt");
 const opponentMoves = {
   Rock: 'A',
   Paper: 'B',
